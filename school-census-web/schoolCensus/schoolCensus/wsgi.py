501f2eb2a61e5a413cea63d51f12455e4ed4f8b4
Muhammad Umer <u.leo86@gmail.com>
2017-01-12 20:54:12 +0500

    try to run app on production

--- school-census-web/schoolCensus/schoolCensus/wsgi.py
+++ school-census-web/schoolCensus/schoolCensus/wsgi.py
@@ -1,6 +1,6 @@
 import sys
 
-path = '/home/wyounas/punjab-school-census-analysis/school-census-web/schoolCensus'
+path = '/home/wyounas/punjab-school-census-analysis/school-census-web/schoolCensus/schoolCensus'
 if path not in sys.path:
    sys.path.append(path)
 
